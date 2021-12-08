45c78770f80e728e6a8cb5a2efc90f73e9112930
Carl Düvel <c.a.duevel@gmail.com>
2021-12-08 08:09:03 +0100

    fix

--- lib/utils.bash
+++ lib/utils.bash
@@ -60,7 +60,6 @@ install_version() {
     mkdir -p "$install_path"
     cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-    # TODO: Asert tfenv executable exists.
     local tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
     test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be executable."
