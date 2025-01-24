d16efacc09c84b4be52ce075b3b26dd0a970f384
Angel Cancio <7303724+4ng31@users.noreply.github.com>
2025-01-24 15:46:41 -0300

    Updating the files

--- lib/utils.bash
+++ lib/utils.bash
@@ -62,8 +62,8 @@ install_version() {
     local tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
     echo $tool_cmd
-    test -f "$install_path/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be a file."
-    test -x "$install_path/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be executable."
+    echo """ test -f "$install_path/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be a file." """
+    echo """ test -x "$install_path/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be executable." """
 
     echo "$TOOL_NAME $version installation was successful!"
   ) || (
